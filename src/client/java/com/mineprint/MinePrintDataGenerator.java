df0823ae4264d360c5dec7f5355c93d42465a2cd
AustinLammers <austinlammers11@gmail.com>
2026-03-01 13:39:32 -0700

    init

---
+++ src/client/java/com/mineprint/MinePrintDataGenerator.java
@@ -0,0 +1,11 @@
+package com.mineprint;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinePrintDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
