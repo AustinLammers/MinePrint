5f84ff1ae6470d0ddc2018383770565af5fcb19b
Connor Brooks <connormbrooks@icloud.com>
2026-03-01 13:15:26 -0700

    jobs done!

---
+++ MinePrint/src/client/java/org/ktpMinePrint/mineprint/client/MineprintClient.java
@@ -0,0 +1,10 @@
+package org.ktpMinePrint.mineprint.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineprintClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ MinePrint/src/main/java/org/ktpMinePrint/mineprint/Mineprint.java
@@ -0,0 +1,10 @@
+package org.ktpMinePrint.mineprint;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mineprint implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
