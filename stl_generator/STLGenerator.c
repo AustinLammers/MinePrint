62a966db6f477d97e01900cbc56e2aaafd1bb916
AustinLammers <austinlammers11@gmail.com>
2026-03-15 14:10:20 -0600

    stl generator start

---
+++ stl_generator/STLGenerator.c
@@ -0,0 +1,55 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "STLGenerator.h"
+#include "string.h"
+
+int main (int argc, char** argv) {
+
+    if (argc < 2) {
+        printf("No input file specified.\n");
+        return -1;
+    }
+
+}
+
+blockNode_t* ingestFile(char* filepath) {
+
+    FILE * file = fopen(filepath, "r");
+    if (!file) {
+        printf("We couldn't find '%s'", filepath);
+        return NULL;
+    }
+
+    blockNode_t * list = NULL;
+
+    int blocksCreated = 0;
+    int lineLength = 0;
+    char line[CSV_MAX_LINE_LENGTH] = {0};
+
+    while ((lineLength = fscanf(file, "%s", &line[0])) > 0) {
+        blockNode_t * block = (blockNode_t *) malloc(sizeof(blockNode_t));
+        if (blocksCreated == 0) {
+            list = block;
+        }
+
+        int cols = extractBlockData(&block->info, line);
+
+        if (cols != CSV_COLUMNS ) {
+            printf("error processing line %d in file\n", blocksCreated + 1); 
+            printf("\tproc'd errant number of cols: %d\n", cols);
+            DEBUG("\tline=%s\n", line);
+        }
+
+
+
+    }
+}
+
+int extractBlockData(block_t* block, char* csvLine) {
+    if(strlen(csvLine) <= CSV_COLUMNS) {
+        return 0;
+    }
+
+    return sscanf(csvLine, "%d, %d, %d", &block->x, &block->y, &block->z);
+
+}
---
+++ stl_generator/STLGenerator.h
@@ -0,0 +1,43 @@
+#ifndef STLGENERATOR_H
+#define STLGENERATOR_H
+
+#define DEBUG(...) printf(__VA_ARGS__)
+
+#define CSV_MAX_LINE_LENGTH 128
+#define CSV_COLUMNS 3
+
+/* Face identifier definitions */
+#define BLOCK_FACE_PLUS_X (0x1 << 0)
+#define BLOCK_FACE_MINUS_X (0x1 << 1)
+#define BLOCK_FACE_PLUS_Y (0x1 << 2)
+#define BLOCK_FACE_MINUS_Y (0x1 << 3)
+#define BLOCK_FACE_PLUS_Z (0x1 << 4)
+#define BLOCK_FACE_MINUS_Z (0x1 << 5)
+#define BLOCK_FACE_INVALID (0x3 << 6)
+
+/* Structure to hold information about a block's position and faces */
+typedef struct {
+    int x;
+    int y; 
+    int z;
+    char faces;
+} block_t;
+
+/* Structure defining a block linked list node */
+typedef struct {
+     block_t info;
+     blockNode_t * next;
+} blockNode_t;
+
+/* This function takes a file path to a csv file as input 
+    and returns a linked list of block data */
+blockNode_t* ingestFile(char* filepath);
+
+/** extract the block data from a CSV line, store it at the block pointer.
+    returns the number of columns successfully processed */
+int extractBlockData(block_t* block, char* csvLine);
+
+/* Get the renderable faces for each block, returns the number of faces populated */
+int calculateRenderedFaces(blockNode_t * list);
+
+#endif
